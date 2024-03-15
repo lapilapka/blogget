b42a9b50fc97e83e564bc328124e2599573ef7c9
e.belousova <e.belousova@finflowsoft.com>
2024-03-16 00:41:45 +0300

    Add eslint and proptypes

--- src/components/Header/Heading/Heading.jsx
+++ src/components/Header/Heading/Heading.jsx
@@ -1,7 +1,3 @@
 import style from './Heading.module.css';
 
-export const Heading = () => {
-  return (
-      <h1 className={style.heading}>Главная</h1>
-  )
-}
+export const Heading = () => <h1 className={style.heading}>Главная</h1>;
